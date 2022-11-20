1045ea788e7993a287e8c3b47a210148f72c6f4c
Sheen <mrprofessoracc@gmail.com>
2022-11-20 16:38:53 +0100

    Add webdrive

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+
+# webdriver and options
+options = Options()
+options.add_argument("--start-maximized")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-notifications")
+options.headless = False
+options.page_load_strategy = "normal"
+webdriver = webdriver.Chrome(options=options)
