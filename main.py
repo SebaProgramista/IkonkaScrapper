163e80752e1c26e5cdec2710f58b1d090aecd84d
Sheen <mrprofessoracc@gmail.com>
2022-11-20 16:33:01 +0100

    Add selenium imports

---
+++ main.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
